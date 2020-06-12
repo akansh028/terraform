5c4c833ff64be0b2e68b02da12ef8db842e71cc0
Akansh Agarwal <55538907+akansh028@users.noreply.github.com>
2020-06-12 22:46:49 +0530

    Update index.php

--- index.php
+++ index.php
@@ -5,7 +5,7 @@ print `ifconfig`;
 echo ' 
 <!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01//EN" "http://www.w3.org/TR/html4/strict.dtd">
 <html>
-<img src="C:/Users/akans/Downloads/terraform.png" width="280" height="125" title="Terraform" alt="Terraform" />
+<img src="terraform.png" width="280" height="125" title="Terraform" alt="Terraform" />
 
 </html>
 ';
